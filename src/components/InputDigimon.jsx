dfac0ea4e8cbaa0e6c634fcef6dd0d29fccf84fe
aegarmus <aegarmus@gmail.com>
2024-10-12 13:52:36 -0300

    feat: Components and hook for fetch one Digimon

---
+++ src/components/InputDigimon.jsx
@@ -0,0 +1,11 @@
+export const InputDigimon = () => {
+    return (
+        <div className="input-digimon">
+            <input 
+                type="text"
+                placeholder="Que compañero Digimon Buscas" 
+                
+            />
+        </div>
+    )
+}
--- src/services/apiDigimon.js
+++ src/services/apiDigimon.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+
+const BASE_URL = "https://digi-api.com/api/v1"
+
 export const fetchDigimons = async() => {
     try {
-        const { data } = await axios.get("https://digi-api.com/api/v1/digimo");
+        const { data } = await axios.get(`${BASE_URL}/digimon`);
         if(!data) throw new Error('Error al capturar los datos de los digimons')
         return data
     } catch (error) {
@@ -10,6 +13,15 @@ export const fetchDigimons = async() => {
     }
 }
 
+export const fetchOneDigimon = async(digimon) => {
+    try {
+        const { data } = await axios.get(`${BASE_URL}/digimon/${digimon}`)
+        if(!data) throw new Error(`Error al encontrar el digimon ${digimon}`)
+        return data
+    } catch (error) {
+        throw new Error(`Error al encontrar el digimon ${digimon}`, error)
+    }
+}
 
 //thruthy => {}, []
 //falsy => false, 0, '', null, undefined, NaN
